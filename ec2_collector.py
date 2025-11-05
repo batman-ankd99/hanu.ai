4f8a02c8b05c0bc3f86e62e826655b80315074ec
ankur <ankd99@gmail.com>
2025-11-06 00:47:23 +0530

    ec2 details push to postgres

--- ec2_collector.py
+++ ec2_collector.py
@@ -4,36 +4,83 @@ from datetime import datetime
 from dotenv import load_dotenv #to load .env files key value in enviroment of app
 import os
 
-"""Collect AWS EC2 details and store them in PostgreSQL."""
-load_dotenv(".env.prod")        #loads key, value from .env.prod file in os env var
+def collect_ec2_data():
+    """Collect AWS EC2 details and store them in PostgreSQL."""
+    load_dotenv(".env.prod")        #loads key, value from .env.prod file in os env var
 
 
-##Postgress DB connections details
-db_host=os.getenv("DB_HOST")
-db_name=os.getenv("DB_NAME")
-db_user=os.getenv("DB_USER")
-db_pass=os.getenv("DB_PASS")
+    ##Postgress DB connections details
+    db_host=os.getenv("DB_HOST")
+    db_name=os.getenv("DB_NAME")
+    db_user=os.getenv("DB_USER")
+    db_pass=os.getenv("DB_PASS")
 
 
-##Fetch Ec2 details of account and push them to DB name
+    ##Fetch Ec2 details of account and push them to DB name
 
-ec2_client=boto3.client('ec2') #to be used for ec2 and sg info
+    ec2_client=boto3.client('ec2') #to be used for ec2 and sg info
 
-response = ec2_client.describe_instances()
+    response = ec2_client.describe_instances()
 
-instances = []
-for reservation in response['Reservations']:
-    for ec2 in reservation['Instances']:
-        instance_id = ec2['InstanceId']
-        ip_address =  ec2['PrivateIpAddress']
-        region =   ec2_client.meta.region_name
-        state = ec2['State']['Name']
-        security_groups = []
-        for sg in  ec2['SecurityGroups']:
-            security_groups.append(sg['GroupName'])
-        scan_time = datetime.now()
+    instances = []
+    for reservation in response['Reservations']:
+        for ec2 in reservation['Instances']:
+            instance_id = ec2['InstanceId']
+            ip_address =  ec2['PrivateIpAddress']
+            region =   ec2_client.meta.region_name
+            state = ec2['State']['Name']
+            security_groups = []
+            for sg in  ec2['SecurityGroups']:
+                security_groups.append(sg['GroupName'])
+            scan_time = datetime.now()
 
-        instances.append((instance_id, ip_address, region, state, security_groups, scan_time, ip_address))
-#print(instances)   ##double brackets means - for every instance all fields are added as a list in list instances
+            instances.append((instance_id, ip_address, region, state, security_groups, scan_time, ip_address))
+    #print(instances)   ##double brackets means - for every instance all fields are added as a list in list instances
 
-##Connect to DB now using above details
+    ##Connect to DB now using above details
+
+    try:
+
+        conn = psycopg2.connect(
+                host=db_host,
+                database=db_name,
+                user=db_user,
+                password=db_pass
+                )
+
+        cursor = conn.cursor()
+        print("Database connected")
+
+    except Exception as e:
+        print("Database connection failed",e)
+
+
+
+    ##sql query
+    #INSERT into ec2_instances (instance_id, instance_name, state, region, security_groups, scan_time, pvt_ip) VALUES ('asdhbs', 'nameiinstance', 'up', 'us-east-1', 'default',  '2025-11-03 11:42:28', '10.100.0.1')
+    ##
+
+    insert_query = """
+     INSERT into ec2_instances (instance_id, instance_name, state, region, security_groups, scan_time, pvt_ip) VALUES (%s, %s, %s, %s, %s, %s, %s)
+
+     ON CONFLICT (instance_id)
+     DO UPDATE SET
+        instance_name = EXCLUDED.instance_name,
+        state = EXCLUDED.state,
+        region = EXCLUDED.region,
+        security_groups = EXCLUDED.security_groups,
+        scan_time = EXCLUDED.scan_time,
+        pvt_ip = EXCLUDED.pvt_ip;
+    """
+
+    for instance_info in instances:
+        cursor.execute(insert_query, tuple(instance_info))
+
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+# Allow running directly or importing in collector.py file
+if __name__ == "__main__":
+    collect_ec2_data()
