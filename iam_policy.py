3cf5d88ae18a6c7d2c6623c849ee58abe969ace3
ankur <ankd99@gmail.com>
2025-11-07 20:46:20 +0530

    remove function to make executable

--- iam_policy.py
+++ iam_policy.py
@@ -4,9 +4,9 @@ from datetime import datetime
 from dotenv import load_dotenv #to load .env files key value in enviroment of app
 import os
 
-def collect_iampolicy_data():
-    """Collect AWS IAM Custom Policies details and store them in PostgreSQL."""
-    load_dotenv(".env.prod")
 
-    iam = boto3.client('iam')
-    response = iam.get_policy
+"""Collect AWS IAM Custom Policies details and store them in PostgreSQL."""
+load_dotenv(".env.prod")
+
+iam = boto3.client('iam')
+response = iam.get_policy    
