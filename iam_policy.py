4790f348fbb664383d48bf0d552e76c70523918c
ankur <ankd99@gmail.com>
2025-11-07 22:05:46 +0530

    DB insertion

--- iam_policy.py
+++ iam_policy.py
@@ -4,56 +4,67 @@ from dotenv import load_dotenv
 import os
 import json
 
-# Load environment variables
-load_dotenv(".env.prod")
+def collect_s3_data():
+    """Collect AWS S3 bucket details and store them in PostgreSQL."""
+    load_dotenv(".env.prod")
 
-iam = boto3.client('iam')
+    ##Postgres DB connections details
+    db_host = os.getenv("DB_HOST")
+    db_name = os.getenv("DB_NAME")
+    db_user = os.getenv("DB_USER")
+    db_pass = os.getenv("DB_PASS")
 
-response = iam.list_policies(Scope='All')
+    iam = boto3.client('iam')
 
-policy_list = []
-iam_data = []
+    response = iam.list_policies(Scope='All')
 
-policy_list_allinfo = response['Policies']
+    policy_list = []
+    iam_data = []
 
-for policy in policy_list_allinfo:
-    policy_arn = policy['Arn']
-    policy_name = policy['PolicyName']
-    policy_id = policy['PolicyId']
-    create_date = policy.get('CreateDate')
-    update_date = policy.get('UpdateDate')
-    scan_time = datetime.now()
+    policy_list_allinfo = response['Policies']
 
-    # Determine policy type
-    if policy_arn.startswith("arn:aws:iam::aws:policy/"):
-        policy_type = "aws_managed"
-    else:
-        policy_type = "customer_managed"
+    for policy in policy_list_allinfo:
+        policy_arn = policy['Arn']
+        policy_name = policy['PolicyName']
+        policy_id = policy['PolicyId']
+        create_date = policy.get('CreateDate')
+        update_date = policy.get('UpdateDate')
+        scan_time = datetime.now()
 
-    # Collect attached entities (groups, users, roles)
-    entities = iam.list_entities_for_policy(PolicyArn=policy_arn)
-    attached_groups = [g['GroupName'] for g in entities['PolicyGroups']]
-    attached_users = [u['UserName'] for u in entities['PolicyUsers']]
-    attached_roles = [r['RoleName'] for r in entities['PolicyRoles']]
+        # Determine policy type
+        if policy_arn.startswith("arn:aws:iam::aws:policy/"):
+            policy_type = "aws_managed"
+        else:
+            policy_type = "customer_managed"
 
-    entity_list = {
-        "Groups": attached_groups,
-        "Users": attached_users,
-        "Roles": attached_roles
-    }
+        # Collect attached entities (groups, users, roles)
+        entities = iam.list_entities_for_policy(PolicyArn=policy_arn)
+        attached_groups = [g['GroupName'] for g in entities['PolicyGroups']]
+        attached_users = [u['UserName'] for u in entities['PolicyUsers']]
+        attached_roles = [r['RoleName'] for r in entities['PolicyRoles']]
 
-    # Append tuple of policy info
-    iam_data.append((
-        policy_arn,
-        policy_name,
-        policy_id,
-        policy_type,
-        json.dumps(entity_list),
-        create_date,
-        update_date,
-        scan_time
-    ))
+        entity_list = {
+            "Groups": attached_groups,
+            "Users": attached_users,
+            "Roles": attached_roles
+        }
 
-# Print collected data
-for record in iam_data:
-    print(record)
+        # Append tuple of policy info
+        iam_data.append((
+            policy_arn,
+            policy_name,
+            policy_id,
+            policy_type,
+            json.dumps(entity_list),
+            create_date,
+            update_date,
+            scan_time
+        ))
+
+    # Print collected data
+    for record in iam_data:
+        print(record)
+
+# Allow running directly or from collector.py
+if __name__ == "__main__":
+    collect_s3_data()        
