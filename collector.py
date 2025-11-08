a6ed48d5bbf5cabd8fbca2f09afa0965dc455559
ankur <ankd99@gmail.com>
2025-11-08 16:54:59 +0530

    collector.py fixes

--- collector.py
+++ collector.py
@@ -1,7 +1,7 @@
 from ec2_collector import collect_ec2_data
 from sg_collector import collect_sg_data
 from s3_collector import collect_s3_data
-from iampolicy_collector collect_iampolicy_data
+from iampolicy_collector import collect_iampolicy_data
 
 collect_ec2_data()
 collect_sg_data()
