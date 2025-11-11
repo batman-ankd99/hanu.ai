333a428cd9816f69f3b7f31e5ca4c87e90166ee5
ankur <ankd99@gmail.com>
2025-11-11 20:53:03 +0530

    join query

--- analytics_layer.py
+++ analytics_layer.py
@@ -7,3 +7,18 @@ from db_utils import get_db_connection
 
 conn = get_db_connection()
 cursor = conn.cursor()
+
+insert_query = """
+ SELECT
+     s.id,
+     s.effect,
+     s.principal,
+     s.actions,
+     p.policy_name,
+     p.attached_entities
+ FROM iam_policy_statements AS s
+ JOIN iam_policies AS p
+     ON s.policy_arn = p.policy_arn
+ WHERE (s.is_action_star = 't' OR s.is_principal_star = 't')
+   AND s.effect = 'Allow';
+"""
