097e05a5a83b406648836c966639d7cc565058db
ankur <ankd99@gmail.com>
2025-11-28 21:53:16 +0530

    s3 path

--- flow-log-analysis.py
+++ flow-log-analysis.py
@@ -2,13 +2,20 @@ import boto3
 from datetime import datetime
 import os
 
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+def s3_file_download(year, month, day, bucket_name, aws_ac_num):
 
-def s3_file_download(year, month, day, bucket_name):
+    prefix = f"AWSLogs/{aws_ac_num}/vpcflowlogs/us-east-1/{year}/{month}/{day}/"
+    local_folder = f"/opt/{year}-{month:02d}-{day:02d}"
+    os.makedirs(local_folder, exist_ok=True)
 
-    filepath= f"vpc-flow-log-hanu/AWSLogs/426728253870/vpcflowlogs/us-east-1/{year}/{month}/{day}"
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    for obj in response['Contents']:
+        key = obj['Key']
+        filename= key.split("/")[-1]
+        local_path = os.path.join(local_folder, filename)
 
-s3_file_download(2025, 11, 24, "vpc-flow-log-hanu")
+        s3.download_file(bucket_name, key, local_path)
+
+s3_file_download(2025, 11, 24, "vpc-flow-log-hanu", 426728253870)
